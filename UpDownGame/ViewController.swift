3f036d3093a17cb8b1780f64a290595bf57a5baf
Subin Lee <chandar.sblee@gmail.com>
2020-08-16 13:34:07 +0900

    first commit

---
+++ UpDownGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UpDownGame
+//
+//  Created by 이수빈 on 2020/08/12.
+//  Copyright © 2020 subux. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
